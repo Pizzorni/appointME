8e9efcdbc261f8b22779e7ecb6a7b7fd1e6581a8
Giorgio Pizzorni <Giorgio@Pizzorni.com>
2015-12-10 22:50:19 +0000

    changed test view

--- scheduler/views.py
+++ scheduler/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World. Welcome to the scheduler index.")
+    return HttpResponse("Hello, World. You win.")
