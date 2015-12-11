804401f3f1953036bf56c3292200e37706a2edab
Giorgio Pizzorni <Giorgio@Pizzorni.com>
2015-12-11 20:20:44 +0000

    just a test. please ignore

--- scheduler/views.py
+++ scheduler/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World. You win.")
+    return HttpResponse("Hello World, can you hear me? I'm in New York dreaming about who I used to be")
