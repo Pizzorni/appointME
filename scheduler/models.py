018fd74bfdd2ab0d05a4077995f8b7db9a3d1b67
Giorgio Pizzorni <Giorgio@Pizzorni.com>
2015-12-12 20:07:54 +0000

    added user creation form

--- scheduler/models.py
+++ scheduler/models.py
@@ -13,43 +13,6 @@ class Student(models.Model):
     majorTwo = models.CharField(max_length = 8, default = "None")
     minor = models.CharField(max_length = 8, default = "None")
     year_in_school = models.CharField(max_length=20, default = "Freshman")
-
-    def getName(self):
-        return self.user.get_full_name()
-    def getGPA(self):
-        return self.gpa
-    def getYear(self):
-        return self.year_in_school
-    def getMajorOne(self):
-        return self.majorOne
-    def getMajorTwo(self):
-        return self.majorTwo
-    def getMajors(self):
-        majors = []
-        majors.append(self.majorOne)
-        majors.append(self.majorTwo)
-        return majors
-    def getMinor(self):
-        return self.minor
-    def printName(self):
-        return "Name:", self.user.get_full_name()
-    def printGPA(self):
-        return "GPA:", self.gpa
-    def printMajors(self):
-        return "First Major:" , self.majorOne, "Second Major:", self.majorTwo
-    def printMinors(self):
-        return "Minor:", self.minor
-    def printYear(self):
-        return "Academic Year:", self.year_in_school
-    def printStudentInfo(self):
-        name = printName()
-        gpa = printGPA()
-        majors = printMajors()
-        minor = printMinors()
-        year = printYear()
-        return name + "\n" + gpa + "\n" + majors + "\n" + minors + "\n" + year 
-
-
     
 class Advisor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -63,7 +26,11 @@ class Appointment(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
-
+class MyUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        help_texts = {
+            'username' : '',
+        }
 
 
 
