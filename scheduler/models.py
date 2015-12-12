d9e7067edc120032e37328b569fb9619e25b6d55
Giorgio Pizzorni <Giorgio@Pizzorni.com>
2015-12-12 15:17:15 +0000

    simplified

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,44 +1,34 @@
 from __future__ import unicode_literals
-
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
+class Subjects(models.Model):
+#    COMPSCI = 'CS'
+#    MATH = 'MATH'
+#    ECONOMICS = 'ECON'
+#    ENGLISH = 'ENGL'
+#    NONE = 'NONE'
+#    MAJOR_CHOICES = (
+#        (MATH, 'Mathematics'),
+#        (COMPSCI, 'Computer Science'),
+#        (ECONOMICS, 'Economics'),
+#        (ENGLISH, 'English'),
+#       (NONE, 'None'),
+#    )
+    major = models.CharField(max_length = 8, default = "None")
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     gpa = models.DecimalField(max_digits = 3, decimal_places = 2)
 
-    COMPSCI = 'CS'
-    MATH = 'MATH'
-    ECONOMICS = 'ECON'
-    ENGLISH = 'ENGL'
-    NONE = 'NONE'
-    MAJOR_CHOICES = (
-        (MATH, 'Mathematics'),
-        (COMPSCI, 'Computer Science'),
-        (ECONOMICS, 'Economics'),
-        (ENGLISH, 'English'),
-        (NONE, 'None'),
-    )
 
-
-    majorOne = models.CharField(max_length = 4, choices = MAJOR_CHOICES, default = ENGLISH)
-    majorTwo = models.CharField(max_length = 4, choices = MAJOR_CHOICES, default = NONE)
-    minor = models.CharField(max_length = 4, choices = MAJOR_CHOICES, default = NONE)
-
-    FRESHMAN = 'FR'
-    SOPHOMORE = 'SO'
-    JUNIOR = 'JR'
-    SENIOR = 'SR'
-    YEAR_IN_SCHOOL_CHOICES = (
-        (FRESHMAN, 'Freshman'),
-        (SOPHOMORE, 'Sophomore'),
-        (JUNIOR, 'Junior'),
-        (SENIOR, 'Senior'),
-    )
-    year_in_school = models.CharField(max_length=2, choices = YEAR_IN_SCHOOL_CHOICES, default = FRESHMAN)
+    majorOne = models.ManyToManyField(Subjects, related_name="majorOne")
+    majorTwo = models.ManyToManyField(Subjects, related_name ="majorTwo")
+    minor = models.ManyToManyField(Subjects)
+    year_in_school = models.CharField(max_length=20, default = "Freshman")
 
     def getName(self):
         return self.user.get_full_name()
@@ -76,10 +66,11 @@ class Student(models.Model):
         return name + "\n" + gpa + "\n" + majors + "\n" + minors + "\n" + year 
 
 
+    
 class Advisor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     officeLocation =  models.CharField(max_length = 20)
-
+    specialty = models.ForeignKey(Subjects, null = True, on_delete = models.CASCADE)
 
 #class Posting(models.Model):
 
