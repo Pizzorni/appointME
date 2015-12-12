f28dfb020290758b7031abaea7a7ee35f74ce69f
Dexter Tong <dyt217@nyu.edu>
2015-12-12 12:20:29 -0500

    added registration

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
    url(r'^index', views.index, name='index'),
    url(r'^login', views.login, name='login'),
+   url(r'^register', views.register, name='register'),
    #url(r'^advisorCalendar', views.advisorCalendar, name='advisorCalendar'),
    #url(r'^studentCalendar', views.studentCalendar, name='studentCalendar'),
    url(r'^calendar', views.advisorCalendar, name='advisorCalendar'),
